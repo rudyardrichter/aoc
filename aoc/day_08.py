b094060e6ce4ff39860de7cbcc30e128b63fd203
Rudyard Richter <rudyardrichter@uchicago.edu>
2019-12-28 13:45:36 -0600

    Finish Day 8 Part 2

--- aoc/day_08.py
+++ aoc/day_08.py
@@ -2,12 +2,30 @@ import numpy
 
 
 class Image:
+    colormap = {
+        0: "█",
+        1: "░",
+    }
+
     def __init__(self, w: int, h: int, pixels: numpy.ndarray):
         self.layers = numpy.array(pixels).reshape((len(pixels) // (w * h), h, w))
 
     @classmethod
     def from_str(cls, w: int, h: int, data: str):
         return cls(w, h, list(map(int, data.strip())))
+
+    def composite(self) -> numpy.ndarray:
+        def render(stack):
+            return stack[numpy.argmax(stack < 2)]
+        return numpy.apply_along_axis(render, 0, self.layers)
+
+    def composite_pretty(self, colormap=None) -> str:
+        colormap = colormap or self.colormap
+        composite = self.composite()
+        printout = numpy.ndarray(composite.shape, dtype=str)
+        for k, v in colormap.items():
+            printout = numpy.where(composite == k, v, printout)
+        return "\n".join("".join(l) for l in printout.tolist())
 
 
 def part_1(data: str) -> int:
@@ -16,3 +34,7 @@ def part_1(data: str) -> int:
     layer = image.layers.take(indices=i, axis=0)
     counts = dict(zip(*numpy.unique(layer, return_counts=True)))
     return counts[1] * counts[2]
+
+
+def part_2(data: str) -> int:
+    return Image.from_str(25, 6, data).composite_pretty()
--- tests/test_image.py
+++ tests/test_image.py
@@ -4,3 +4,9 @@ from aoc.day_08 import Image
 def test_image_indices():
     image = Image.from_str(3, 2, "123456789012")
     assert image.layers[0][1][0] == 4
+
+
+def test_composite():
+    image = Image.from_str(2, 2, "0222112222120000")
+    assert image.composite().tolist() == [[0, 1], [1, 0]]
+    assert image.composite_pretty() == "█░\n░█"
