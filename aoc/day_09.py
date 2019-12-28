eaf06a5808c06c5d1b259068c09faac9c7306de9
Rudyard Richter <rudyardrichter@uchicago.edu>
2019-12-28 14:52:11 -0600

    Finish Day 9 Part 2

--- aoc/day_09.py
+++ aoc/day_09.py
@@ -5,3 +5,9 @@ def part_1(data: str) -> int:
     c = Computer.from_str(data).send_input(1)
     c.execute()
     return c.outputs[-1]
+
+
+def part_2(data: str) -> int:
+    c = Computer.from_str(data).send_input(2)
+    c.execute()
+    return c.outputs[-1]
