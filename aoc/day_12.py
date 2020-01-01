5b53ecf2e87f6c31a9739413b74ee321a7151a8b
Rudyard Richter <rudyardrichter@uchicago.edu>
2020-01-01 15:55:42 -0600

    Finish Day 12 Part 2

--- aoc/day_12.py
+++ aoc/day_12.py
@@ -33,13 +33,27 @@ class System:
         k = numpy.sum(numpy.absolute(self.v), axis=1)
         return numpy.sum(u * k)
 
+    def period(self) -> int:
+        """
+        very
+        slow
+        """
+        period = numpy.zeros(self.x.shape[1], dtype=int)
+        self.step()
+        x_0 = numpy.copy(self.x)
+        v_0 = numpy.copy(self.v)
+        i = 0
+        while not period.all():
+            self.step()
+            i += 1
+            match = numpy.concatenate((self.x == x_0, self.v == v_0)).all(axis=0)
+            period[numpy.argwhere(numpy.logical_and(match, period == 0))] = i
+        return numpy.lcm.reduce(period)
+
 
 def part_1(data: str) -> int:
     return System.from_str(data).step(1000).energy
 
 
 def part_2(data: str) -> int:
-    # TODO
-    # - get period for each dimension
-    # - take lcm of all three dimensions
-    pass
+    return System.from_str(data).period()
--- tests/test_nbody.py
+++ tests/test_nbody.py
@@ -3,30 +3,29 @@ import pytest
 from aoc.day_12 import System
 
 
+examples = [
+    "<x=-1, y=0, z=2>\n<x=2, y=-10, z=-7>\n<x=4, y=-8, z=8>\n<x=3, y=5, z=-1>\n",
+    "<x=-8, y=-10, z=0>\n<x=5, y=5, z=10>\n<x=2, y=-7, z=3>\n<x=9, y=-8, z=-3>\n",
+]
+
+
 @pytest.mark.parametrize(
     ("data", "steps", "energy"),
     [
-        (
-            (
-                "<x=-1, y=0, z=2>\n"
-                "<x=2, y=-10, z=-7>\n"
-                "<x=4, y=-8, z=8>\n"
-                "<x=3, y=5, z=-1>\n"
-            ),
-            10,
-            179,
-        ),
-        (
-            (
-                "<x=-8, y=-10, z=0>\n"
-                "<x=5, y=5, z=10>\n"
-                "<x=2, y=-7, z=3>\n"
-                "<x=9, y=-8, z=-3>\n"
-            ),
-            100,
-            1940,
-        )
-    ]
+        (examples[0], 10, 179),
+        (examples[1], 100, 1940),
+    ],
 )
 def test_system_energy(data, steps, energy):
     assert System.from_str(data).step(steps).energy == energy
+
+
+@pytest.mark.parametrize(
+    ("data", "period"),
+    [
+        (examples[0], 2772),
+        (examples[1], 4686774924),
+    ],
+)
+def test_system_period(data, period):
+    assert System.from_str(data).period() == period
